cf9e41f1c40565682b0949638ec2c11f71e63052
ostrygun <oleg_strygun@intuit.com>
2024-06-06 15:46:33 -0700

    pretty TAPIR loger name

--- tapir-quartz-h2/src/main/scala/io/quartz/sttp/QuartzH2DefaultServerLog.scala
+++ tapir-quartz-h2/src/main/scala/io/quartz/sttp/QuartzH2DefaultServerLog.scala
@@ -4,11 +4,12 @@ import cats.effect.Sync
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import sttp.tapir.server.interceptor.log.DefaultServerLog
+import org.typelevel.log4cats.LoggerName
 
 object QuartzH2DefaultServerLog {
 
   def apply[F[_]: Sync]: DefaultServerLog[F] = {
-    val log = Slf4jLogger.getLogger[F]
+    val log = Slf4jLogger.getLogger[F](using cats.effect.kernel.Sync[F], new LoggerName("tapir"))
     DefaultServerLog(
       doLogWhenReceived = msg => debugLog(log)(msg, None),
       doLogWhenHandled = debugLog(log),
