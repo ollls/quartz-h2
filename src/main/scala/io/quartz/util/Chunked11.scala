28bc368af9e7229c5f226cd4ff263e72cd9eb9c5
Oleg Strygun <ostrygun@gmail.com>
2025-03-22 19:42:00 -0700

    http 11 cleanup with AI

--- src/main/scala/io/quartz/util/Chunked11.scala
+++ src/main/scala/io/quartz/util/Chunked11.scala
@@ -2,85 +2,157 @@ package io.quartz.util
 import fs2.{Stream, Pull, Chunk}
 import cats.effect.IO
 
-
+/**
+ * HTTP/1.1 Chunked Transfer Encoding Handler
+ * ----------------------------------------
+ * Implements RFC 7230 compliant chunked transfer encoding processing.
+ * Efficiently processes chunked HTTP data streams with proper validation.
+ * 
+ * Key features:
+ * - Chunk size extraction and validation
+ * - Proper handling of chunk terminators (CRLF)
+ * - Stream-based processing for memory efficiency
+ * - Error handling for malformed chunks
+ */
 sealed case class ChunkedEncodingError(msg: String) extends Exception(msg)
 
 object Chunked11 {
 
-  /////////////////////////////////////////////////////////////////
-  def chunk_drop_with_validation(data: Chunk[Byte], offset: Int) = {
+  /**
+   * Validates and drops chunk terminators
+   * @param data The chunk data
+   * @param offset Position to start validation from
+   * @return Remaining data after removing chunk terminators
+   * @throws ChunkedEncodingError if chunk terminator is invalid
+   */
+  def chunk_drop_with_validation(data: Chunk[Byte], offset: Int): Chunk[Byte] = {
     val data2 = data.drop(offset)
-    // validate end of chunk
-    val test = (data2(0) == '\r' && data2(1) == '\n')
-
-    if (test == false) throw (new ChunkedEncodingError(""))
+    // validate end of chunk (CRLF)
+    if (data2.size < 2 || data2(0) != '\r' || data2(1) != '\n') {
+      throw new ChunkedEncodingError("Invalid chunk terminator")
+    }
 
     data2.drop(2) // get rid of end of block markings
-
   }
 
-  ////////////////////////////////////////////////////////////////
+  /**
+   * Extracts chunk length from the beginning of a chunk
+   * @param db Chunk data containing the length header
+   * @return Tuple of (chunk size, offset to chunk data)
+   * @throws ChunkedEncodingError if chunk header is malformed
+   */
   private def extractChunkLen2(db: Chunk[Byte]): (Int, Int) = {
     var l = List.empty[Char]
     var c: Byte = 0
     var i: Int = 0
-    while {
-      { c = db(i); i += 1 }
-      c != '\r' && i < 8 // 8 chars for chunked len
-    } do (l = l.appended(c.toChar))
-    if (c == '\r' && db(i) == '\n') i += 1
-    else throw (new ChunkedEncodingError(""))
-    (Integer.parseInt(l.mkString, 16), i)
+    
+    // Read up to 8 hex characters for the chunk length
+    while (i < db.size && i < 8 && { c = db(i); i += 1; c != '\r' }) {
+      l = l.appended(c.toChar)
+    }
+    
+    // Validate chunk header terminator (CRLF)
+    if (i >= db.size || c != '\r' || i >= db.size + 1 || db(i) != '\n') {
+      throw new ChunkedEncodingError("Malformed chunk header")
+    }
+    
+    i += 1 // Move past '\n'
+    
+    try {
+      (Integer.parseInt(l.mkString, 16), i)
+    } catch {
+      case e: NumberFormatException => 
+        throw new ChunkedEncodingError("Invalid chunk size format")
+    }
   }
 
-  ///////////////////////////////////////////////////////////////
-  def makeChunkedStream(s1: Stream[IO, Byte], timeOutMs: Int) = {
+  /**
+   * Creates a stream that processes HTTP/1.1 chunked encoded data
+   * @param s1 Input stream of bytes
+   * @param timeOutMs Timeout in milliseconds (currently unused)
+   * @return Stream of decoded bytes with chunked encoding removed
+   */
+  def makeChunkedStream(s1: Stream[IO, Byte], timeOutMs: Int): Stream[IO, Chunk[Byte]] = {
 
+    /**
+     * Process a chunk when we already have enough data in the buffer
+     */
     def go2(s: Stream[IO, Byte], hd: Chunk[Byte]): Pull[IO, Byte, Unit] = {
-      val (chunkSize, offset) =
-        try { extractChunkLen2(hd) } //special case when hd ends exactly on the http/11 chunk header, happens with multipart
-        catch { case e: java.lang.IndexOutOfBoundsException => { (hd.size, 0) } }
-      val len = hd.size - offset // actual data avaialble
-      // println("INCOMING2>>>: " + chunkSize.toString() + "  " + offset.toString() + "  " + len)
-      if (chunkSize == 0) Pull.done
-      else if (len == chunkSize + 2)
-        Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go(
-          s,
-          chunk_drop_with_validation(hd, offset + chunkSize)
-        )
-      else if (len >= chunkSize + 2) // account for chunk end marking
-        Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go2(
-          s,
-          chunk_drop_with_validation(hd, offset + chunkSize)
-        )
-      else go(s, hd)
+      try {
+        val (chunkSize, offset) = extractChunkLen2(hd)
+        val len = hd.size - offset // actual data available
+        
+        if (chunkSize == 0) {
+          // Last chunk (size 0) - end of chunked data
+          Pull.done
+        } else if (len == chunkSize + 2) {
+          // We have exactly one chunk plus its terminator
+          Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go(
+            s,
+            chunk_drop_with_validation(hd, offset + chunkSize)
+          )
+        } else if (len >= chunkSize + 2) {
+          // We have at least one chunk plus its terminator and more data
+          Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go2(
+            s,
+            chunk_drop_with_validation(hd, offset + chunkSize)
+          )
+        } else {
+          // Not enough data, need to read more
+          go(s, hd)
+        }
+      } catch {
+        case e: java.lang.IndexOutOfBoundsException => 
+          // Not enough data to extract chunk length
+          go(s, hd)
+        case e: ChunkedEncodingError =>
+          // Propagate chunked encoding errors
+          Pull.raiseError[IO](e)
+      }
     }
 
+    /**
+     * Main processing function that pulls data from the stream
+     */
     def go(s: Stream[IO, Byte], leftover: Chunk[Byte]): Pull[IO, Byte, Unit] = {
       s.pull.uncons.flatMap {
         case Some((hd1, tl)) =>
           val hd = leftover ++ hd1
-          val (chunkSize, offset) =
-            try { extractChunkLen2(hd) } //special case when hd ends exactly on the http/11 chunk header, happens with multipart
-            catch { case e: java.lang.IndexOutOfBoundsException => { (hd.size, 0) } }
-          val len = hd.size - offset // actual data avaialble
-          // println("INCOMING>>>: " + chunkSize.toString() + "  " + offset.toString() + "  " + len)
-          if (chunkSize == 0) Pull.done
-          else if (len == chunkSize + 2)
-            Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go(
-              tl,
-              chunk_drop_with_validation(hd, offset + chunkSize)
-            )
-          else if (len > chunkSize + 2) // account for chunk end marking
-            Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go2(
-              tl,
-              chunk_drop_with_validation(hd, offset + chunkSize)
-            )
-          else go(tl, hd)
+          try {
+            val (chunkSize, offset) = extractChunkLen2(hd)
+            val len = hd.size - offset // actual data available
+            
+            if (chunkSize == 0) {
+              // Last chunk (size 0) - end of chunked data
+              Pull.done
+            } else if (len == chunkSize + 2) {
+              // We have exactly one chunk plus its terminator
+              Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go(
+                tl,
+                chunk_drop_with_validation(hd, offset + chunkSize)
+              )
+            } else if (len > chunkSize + 2) {
+              // We have at least one chunk plus its terminator and more data
+              Pull.output[IO, Byte](hd.drop(offset).take(chunkSize)) >> go2(
+                tl,
+                chunk_drop_with_validation(hd, offset + chunkSize)
+              )
+            } else {
+              // Not enough data, need to read more
+              go(tl, hd)
+            }
+          } catch {
+            case e: java.lang.IndexOutOfBoundsException => 
+              // Not enough data to extract chunk length
+              go(tl, hd)
+            case e: ChunkedEncodingError =>
+              // Propagate chunked encoding errors
+              Pull.raiseError[IO](e)
+          }
         case None => Pull.done
       }
     }
+    
     go(s1, Chunk.empty[Byte]).stream.chunks
-
   }
 }
