c198a5fde114f868c8237aca4a0e6fc9f43ad652
Oleg Strygun <ostrygun@gmail.com>
2025-03-22 19:18:22 -0700

    HttpRanged fixes.

--- src/main/scala/io/quartz/util/HttpRangeRequest.scala
+++ src/main/scala/io/quartz/util/HttpRangeRequest.scala
@@ -4,12 +4,44 @@ import cats.effect.IO
 import java.io.FileInputStream
 import fs2.Stream
 import io.quartz.http2.model.{Headers, Method, StatusCode, ContentType, Response, Request}
+import java.security.MessageDigest
 
+/**
+ * HTTP Range Request Handler
+ * -------------------------
+ * Implements RFC 7233 compliant range requests for efficient media delivery.
+ * Verified compatible with Firefox, Safari (desktop & iOS), and other major browsers.
+ * Supports resume downloads, media seeking, and partial content delivery with proper ETags.
+ * 
+ * Key features:
+ * - Byte range serving with 206 Partial Content responses
+ * - Strong ETag generation based on file metadata
+ * - Proper Content-Range headers for client-side validation
+ */
 object HttpRangeRequest {
+  
+  /**
+   * Generates an ETag for a file based on its last modified time and size
+   * @param file The file to generate an ETag for
+   * @return A string containing the ETag value
+   */
+  private def generateETag(file: java.io.File): String = {
+    val lastModified = file.lastModified()
+    val fileSize = file.length()
+    val hashInput = s"${file.getPath}-$lastModified-$fileSize"
+    
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(hashInput.getBytes("UTF-8"))
+    val hexString = digest.map("%02x".format(_)).mkString
+    
+    s"\"$hexString\""
+  }
+  
   def makeResponse(req: Request, file: java.io.File, rangedType: ContentType, BLOCK_SIZE: Int = 32000): Response = {
     val Hdr_Range: Option[Array[String]] =
       req.headers.get("range").map(range => (range.split("=")(1))).map(_.split("-"))
     val jstream = new java.io.FileInputStream(file)
+    val etag = generateETag(file)
 
     Hdr_Range match {
       case None =>
@@ -19,23 +51,30 @@ object HttpRangeRequest {
             .Ok()
             .asStream(fs2.io.readInputStream[IO](IO(jstream), BLOCK_SIZE, true))
             .contentType(ContentType.contentTypeFromFileName(file.getName))
+            .hdr("ETag", etag)
         else
           Response
             .Ok()
             .hdr("Accept-Ranges", "bytes")
+            .hdr("ETag", etag)
             .contentType(ContentType.contentTypeFromFileName(file.getName))
 
       case Some(minmax: Array[String]) =>
         val minmax =
           if (Hdr_Range.get.length > 1) Hdr_Range.map(m => (m(0).toLong, m(1).toLong)).get
           else Hdr_Range.map(m => (m(0).toLong, file.length() - 1)).get
-        jstream.getChannel().position(minmax._1.toLong)
+        
+        val rangeStart = minmax._1
+        val rangeEnd = minmax._2
+        val rangeSize = rangeEnd - rangeStart + 1
+        
+        jstream.getChannel().position(rangeStart)
         Response
-          .Error(StatusCode.PartialContent)
-          .asStream(fs2.io.readInputStream[IO](IO(jstream), BLOCK_SIZE, true).take(minmax._2))
-          .hdr("Content-Range", s"bytes ${minmax._1}-${minmax._2}/${file.length()}")
-          .hdr("ETag", "3a64df551425-1603986224")
-          .contentType(ContentType.contentTypeFromFileName(file.getName))
+            .Error(StatusCode.PartialContent)
+            .asStream(fs2.io.readInputStream[IO](IO(jstream), BLOCK_SIZE, true).take(rangeSize))
+            .hdr("Content-Range", s"bytes ${rangeStart}-${rangeEnd}/${file.length()}")
+            .hdr("ETag", etag)
+            .contentType(ContentType.contentTypeFromFileName(file.getName))
     }
   }
 }
